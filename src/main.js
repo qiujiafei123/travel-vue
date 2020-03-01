67db83d856d1d24f0ab5d945377fc28072bc6325
邱佳飞 <253896514@qq.com>
2020-03-01 14:37:39 +0800

    轮播图

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'// 解决移动端点击时间有300ms延迟
+import VueAwesomeSwiper from 'vue-awesome-swiper'// 轮播图组件
 import 'styles/reset.css'// 解决移动端适配
 import 'styles/border.css'// 解决移动端1像素边框
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'// 轮播图样式
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.url" />
+      </swiper-slide>
+
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  // props: {
+  //   list: Array
+  // },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '1',
+          url: 'https://pics.lvjs.com.cn/pics/super/2020/02/1581762603_85902.jpg'
+        },
+        {
+          id: '2',
+          url: 'https://pics.lvjs.com.cn/pics/super/2020/02/1581607657_41736.jpg'
+        }
+      ]
+    }
+  }
+  // computed: {
+  //   showSwiper () {
+  //     return this.list.length
+  //   }
+  // }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
