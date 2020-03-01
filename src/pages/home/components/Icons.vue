fe16f4f34fd4b9a1002cf830a04035624de730a8
邱佳飞 <253896514@qq.com>
2020-03-01 16:11:19 +0800

    图标

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="icons">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(page, index) of pages" :key="index">
+          <div class="icon" v-for="item of page" :key="item.id">
+            <div class='icon-img'>
+              <img class='icon-img-content' :src='item.imgUrl' />
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: false
+      },
+      iconList: [
+        {
+          id: '1',
+          imgUrl: 'http://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '景点门票'
+        },
+        {
+          id: '2',
+          imgUrl: 'http://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '滑雪季节'
+        },
+        {
+          id: '3',
+          imgUrl: 'http://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '景点门票景点门票景点门票'
+        },
+        {
+          id: '4',
+          imgUrl: 'http://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '滑雪季节'
+        },
+        {
+          id: '5',
+          imgUrl: 'http://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '景点门票'
+        },
+        {
+          id: '6',
+          imgUrl: 'http://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '滑雪季节'
+        },
+        {
+          id: '7',
+          imgUrl: 'http://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '景点门票'
+        },
+        {
+          id: '8',
+          imgUrl: 'http://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '滑雪季节'
+        },
+        {
+          id: '9',
+          imgUrl: 'http://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '一日游'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icons
+    margin-top: .1rem
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
+</style>
