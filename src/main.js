698e24ebd288c878644245f85700faea75f5f618
邱佳飞 <253896514@qq.com>
2020-03-01 19:24:24 +0800

    等会写

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import fastClick from 'fastclick'// 解决移动端点击时间有300ms延迟
 import VueAwesomeSwiper from 'vue-awesome-swiper'// 轮播图组件
+import store from './store'
 import 'styles/reset.css'// 解决移动端适配
 import 'styles/border.css'// 解决移动端1像素边框
 import 'styles/iconfont.css'
@@ -18,6 +19,7 @@ Vue.use(VueAwesomeSwiper)
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
